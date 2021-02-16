2400eb43ce78c7edb890ebee49eb3bad81a0da7d
klsiewjin1 <siewjin.kunglim@mail.utoronto.ca>
2021-02-16 12:33:07 +0800

    Route appropriate endpoints

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 
-from api.api_v1.endpoints import officer, address
+from api.api_v1.endpoints import officer, address, entity, intermediary, other
 
 api_router = APIRouter()
-api_router.include_router(officer.router, prefix="/officers", tags=["officer"])
 api_router.include_router(address.router, prefix="/addresses", tags=["address"])
+api_router.include_router(entity.router, prefix="/entities", tags=["entity"])
+api_router.include_router(intermediary.router, prefix="/intermediaries", tags=["intermediary"])
+api_router.include_router(officer.router, prefix="/officers", tags=["officer"])
+api_router.include_router(other.router, prefix="/others", tags=["other"])
